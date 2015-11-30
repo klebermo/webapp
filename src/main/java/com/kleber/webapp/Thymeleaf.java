bfcb9a530406cfbb689d99c11ce634049e9773c1
Kleber Mota <klebermo@gmail.com>
2015-11-30 12:44:21 -0300

    thymeleaf configuration class

---
+++ src/main/java/com/kleber/webapp/Thymeleaf.java
@@ -0,0 +1,26 @@
+import com.kleber.webapp;
+
+import java.util.Set;
+import java.util.HashSet;
+
+import org.springframework.context.annotation.Configuration;
+
+import org.thymeleaf.dialect.IDialect;
+import org.thymeleaf.spring3.SpringTemplateEngine;
+import org.thymeleaf.extras.springsecurity3.dialect.SpringSecurityDialect;
+
+@Configuration
+public class Thymeleaf {
+
+  @Bean
+  public SpringTemplateEngine templateEngine() {
+    SpringTemplateEngine engine  =  new SpringTemplateEngine();
+
+    final Set<IDialect> dialects = new HashSet<IDialect>();
+    dialects.add( new SpringSecurityDialect() );
+    engine.setDialects( dialects );
+
+    return engine;
+  }
+
+}
