736168161cf713b18a9e3b20f3773f623b93049a
Kleber Mota <klebermo@gmail.com>
2015-11-30 20:01:16 -0300

    generic class Model

--- src/main/java/com/kleber/webapp/generic/persistence/Model.java
+++ src/main/java/com/kleber/webapp/generic/persistence/Model.java
@@ -5,24 +5,34 @@ import java.util.List;
 import java.util.UUID;
 
 public abstract class Model extends Object {
-	
+
 	public abstract Object getId();
 
 	public String genUUID() {
 		return UUID.randomUUID().toString();
 	}
-	
+
 	public boolean equals(Model object) {
 		return getId().equals(object.getId());
 	}
-	
+
 	public abstract String toString();
-	
+
 	public List<?> getFields() {
 		return Arrays.asList(this.getClass().getFields());
 	}
-	
+
 	public List<?> getMethods() {
 		return Arrays.asList(this.getClass().getMethods());
+	}
+
+	public Object getValue(String field) {
+		String methodName = "get" + field.substring(0, 1).toUpperCase() + field.substring(1, field.length());
+		try {
+			Object value = this.getClass().getMethod(methodName).invoke(this);
+			return value;
+		} catch (Exception e) {
+			return null;
+		}
 	}
 }
