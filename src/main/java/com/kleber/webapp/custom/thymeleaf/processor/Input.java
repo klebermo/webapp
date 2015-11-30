e4e6c8acb40db1641d8eb2d3110f5d5d104fe305
Kleber Mota <klebermo@gmail.com>
2015-11-30 20:02:38 -0300

    processors Input e Textarea

--- src/main/java/com/kleber/webapp/custom/thymeleaf/processor/Input.java
+++ src/main/java/com/kleber/webapp/custom/thymeleaf/processor/Input.java
@@ -8,6 +8,8 @@ import org.thymeleaf.Arguments;
 import org.thymeleaf.dom.Element;
 import org.thymeleaf.dom.Attribute;
 
+import com.kleber.webapp.generic.persistence.Model;
+
 public class Input extends AbstractElementProcessor {
 
   public Input() {
@@ -16,13 +18,14 @@ public class Input extends AbstractElementProcessor {
 
   public ProcessorResult processElement(Arguments arguments, Element element) {
     java.lang.reflect.Field field = (java.lang.reflect.Field) element.getNodeProperty("field");
+    Model target = (Model) arguments.getContext().getVariables().get("command");
 
     Element node = new Element("input");
     for(java.lang.annotation.Annotation annotation : field.getAnnotations())
       if(!annotation.annotationType().getSimpleName().equals("Input") && !annotation.annotationType().getSimpleName().equals("Column"))
         node.setAttribute("type", annotation.annotationType().getSimpleName().toLowerCase());
     node.setAttribute("name", field.getName());
-    node.setAttribute("value", null);
+    node.setAttribute("value", (String) target.getValue(field.getName()));
 
     for( Map.Entry<String, Attribute> entry : element.getAttributeMap().entrySet() )
       node.setAttribute(entry.getKey(), entry.getValue().getValue());
--- src/main/java/com/kleber/webapp/custom/thymeleaf/processor/Textarea.java
+++ src/main/java/com/kleber/webapp/custom/thymeleaf/processor/Textarea.java
@@ -9,6 +9,8 @@ import org.thymeleaf.dom.Element;
 import org.thymeleaf.dom.Attribute;
 import org.thymeleaf.dom.Text;
 
+import com.kleber.webapp.generic.persistence.Model;
+
 public class Textarea extends AbstractElementProcessor {
 
   public Textarea() {
@@ -17,10 +19,11 @@ public class Textarea extends AbstractElementProcessor {
 
   public ProcessorResult processElement(Arguments arguments, Element element) {
     java.lang.reflect.Field field = (java.lang.reflect.Field) element.getNodeProperty("field");
+    Model target = (Model) arguments.getContext().getVariables().get("command");
 
     Element node = new Element("textarea");
     node.setAttribute("name", field.getName());
-    node.addChild(new Text("..."));
+    node.addChild(new Text((String) target.getValue(field.getName())));
 
     for( Map.Entry<String, Attribute> entry : element.getAttributeMap().entrySet() )
       node.setAttribute(entry.getKey(), entry.getValue().getValue());
