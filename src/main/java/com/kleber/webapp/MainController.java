3dd2a210e0824d812cbd7c288867783316bc07ee
Kleber Mota <klebermo@gmail.com>
2015-11-30 23:22:44 -0300

    MainController e MainService

--- src/main/java/com/kleber/webapp/MainController.java
+++ src/main/java/com/kleber/webapp/MainController.java
@@ -6,13 +6,12 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.kleber.webapp.model.usuario.Usuario;
 
 @Controller
 public class MainController {
-	
+
 	@Autowired
 	private MainService serv;
 
@@ -20,21 +19,21 @@ public class MainController {
 	public String index() {
 		return "index";
 	}
-	
+
 	@RequestMapping("/signin")
 	public String signin() {
 		return "signin";
 	}
-	
+
 	@RequestMapping("/signup")
 	public String signup() {
 		return "signup";
 	}
-	
+
 	@RequestMapping(value="/signup", method=RequestMethod.POST)
-	@ResponseBody
-	public void signup_post(@ModelAttribute("object") Usuario object, BindingResult result) {
+	public String signup_post(@ModelAttribute("object") Usuario object, BindingResult result) {
 		serv.signup(object);
-	}	
-	
+		return "signin";
+	}
+
 }
--- src/main/java/com/kleber/webapp/MainService.java
+++ src/main/java/com/kleber/webapp/MainService.java
@@ -1,5 +1,8 @@
 package com.kleber.webapp;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,12 +11,29 @@ import com.kleber.webapp.model.usuario.UsuarioDao;
 
 @Service
 public class MainService {
-	
+
 	@Autowired
 	private UsuarioDao usuario;
-	
+
 	public void signup(Usuario object) {
+		object.setPassword( converte(object.getPassword()) );
 		this.usuario.insert(object);
+	}
+
+	public String converte(String senha) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			md.update(senha.getBytes());
+			byte[] digest = md.digest();
+
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < digest.length; i++)
+					sb.append(Integer.toString((digest[i] & 0xff) + 0x100, 16).substring(1));
+
+			return sb.toString();
+		} catch (Exception e) {
+			return null;
+		}
 	}
 
 }
